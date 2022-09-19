c452a3c41801dcfc0281e45c1e10b2ba41846870
Anastasiia Pimkina <78366371+Anastsiia@users.noreply.github.com>
2022-09-19 23:23:44 +0300

    add Program&Random factory files

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+
+List<int> t = new List<int>();
+t.Add(1);
+t.Add(2);
+t.Add(3);
+t.Add(4);
+t.Add(5);
+
+RandomFactory<int> x = new RandomFactory<int>(t);
+x.RemoveItem(0);
+
+for (int i = 10; i < 50; i++)
+{
+    KeyValuePair<int, int> random = x.GetRandom();
+    Console.WriteLine("Random Key: {0}, Value: {1}", random.Key, random.Value);
+}
+
+Console.ReadLine();
